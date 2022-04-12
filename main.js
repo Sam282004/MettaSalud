fb7a999760d3ddaf7c249e78eb9f2e04b5ee4abd
Samuel Llaccho <89850223+Sam282004@users.noreply.github.com>
2022-04-12 13:35:46 -0500

    Avanze 12-4
    Creamos el lading page :)

---
+++ main.js
@@ -0,0 +1,15 @@
+/*jQuery('document').ready(function($) {
+
+    var btn = $('.menu-icon'),
+        menu = $('.nav ul');
+    btn.click(function() {
+
+        if (menu.hasClass('mostrar')) {
+
+            menu.removeClass('mostrar');
+
+        } else {
+            menu.addClass('mostrar')
+        }
+    })
+});*/
